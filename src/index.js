90e586f857690c44a0e7ae2f601c20c5df911333
oculo0204 <oculo0204@gmail.com>
2025-05-05 00:39:38 +0900

    fix: #6 database connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-const { db } = require('./models/index.js'); 
+import db from './models/index.js'; 
 dotenv.config();
 
 const app = express();
