b6d197549fd96e0fab9e430c239a9b7063a26971
oculo0204 <oculo0204@gmail.com>
2025-05-05 00:46:08 +0900

    fix: Config경로

--- src/models/index.js
+++ src/models/index.js
@@ -10,6 +10,7 @@ import config from "../../config/config.cjs";
 const environmentConfig = config[env];
 
 const __filename = new URL(import.meta.url).pathname; 
+const __dirname = path.dirname(__filename);
 const basename = path.basename(__filename);
 const db = {};
 
