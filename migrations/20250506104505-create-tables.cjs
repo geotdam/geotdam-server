085ef6d1a6b47ec6ad749db39b8b3bb26256ac5d
chaeyeon089 <chaeyeon0091@naver.com>
2025-05-07 17:56:07 +0900

    fix:#14 migration수정

--- migrations/20250506104505-create-tables.cjs
+++ migrations/20250506104505-create-tables.cjs
@@ -14,7 +14,9 @@ module.exports = {
       created_at: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
       updated_at: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
       deleted_at: { type: Sequelize.DATE },
-      status: { type: Sequelize.STRING }
+      status: { type: Sequelize.STRING },
+      kakao_id: { type: Sequelize.STRING, unique: true },
+      email: { type: Sequelize.STRING, allowNull: true, unique: true }
     });
 
     // 2. socialLogins
