64c7e18d0c3a87a7289125ab12ef60ab4496f5e1
oculo0204 <oculo0204@gmail.com>
2025-05-11 22:59:11 +0900

    fix: auth.controller

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import SocialLoginService from "../../services/socialLogin/socialLogin.service.js";
-=======
 //auth.controller.js
 import SocialLoginService from '../../services/socialLogin/socialLogin.service.js';
 
->>>>>>> main
 const service = new SocialLoginService();
 
 export const kakaoLogin = async (req, res) => {
@@ -31,22 +27,6 @@ export const kakaoLoginRedirect = (req, res) => {
 // 새로 추가: 브라우저 리디렉션 처리용
 export const kakaoCallback = async (req, res) => {
   try {
-<<<<<<< HEAD
-    const code = req.query.code;
-    const result = await service.kakaoLogin(code);
-    // ✅ 리디렉션 후 토큰 반환 or 메인 페이지 이동 가능
-    res.status(200).json(result);
-    // 또는:
-    // res.redirect(`http://localhost:3000/your-frontend?token=${result.token}`);
-  } catch (error) {
-    console.error(
-      "❌ GET 카카오 콜백 오류:",
-      error.response?.data || error.message
-    );
-    res.status(500).json({ message: "카카오 로그인 실패" });
-  }
-};
-=======
     const { code, state = 'normal' } = req.query;
     const result = await service.kakaoLogin(code, state);
 
@@ -63,4 +43,3 @@ export const kakaoCallback = async (req, res) => {
     return res.status(500).json({ message: '카카오 로그인 실패' });
   }
 };
->>>>>>> main
