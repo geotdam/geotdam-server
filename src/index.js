66d47488ee5914eb6a467f1d2c6e8ea6ff0aa084
gimchaewon <gimchaewon417@gmail.com>
2025-05-12 10:02:16 +0900

    🔀 merge: 충돌 처리

--- src/index.js
+++ src/index.js
@@ -16,14 +16,9 @@ app.use(cors()); // cors 방식 허용
 app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
-<<<<<<< HEAD
-app.use('/api/auth', authRoutes);
-app.use('/api/routes', routeRoutes);
-=======
-
 app.use("/api/auth", authRoutes); // auth 라우터 등록
+app.use('/api/routes', routeRoutes);
 
->>>>>>> 71de13f71b43fa181d800b14f0647de9a0e380e1
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
