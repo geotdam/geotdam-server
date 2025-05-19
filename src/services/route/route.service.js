755849047b85157bba8fde8144abd8a55123fb34
gimchaewon <gimchaewon417@gmail.com>
2025-05-19 23:54:36 +0900

    ♻️ refactor:   경로, 장소 검색 에러 핸들링 추가

--- src/services/route/route.service.js
+++ src/services/route/route.service.js
@@ -3,21 +3,26 @@ import axios from 'axios';
 import { searchPlacesFromTmap } from '../maps/place.service.js';
 import { RouteResponseDto } from '../../dtos/route/response/routeResponse.dto.js';
 import { TMAP_API_KEY } from '../../../config/tmap.config.js';
+import {
+  NotExistsError,
+  InvalidInputError,
+  SampleError,
+} from '../../utils/errors/errors.js';
+
 
 const getFirstPlaceCoord = async (placeName) => {
   const places = await searchPlacesFromTmap(placeName);
   if (!places.length) {
-    const error = new Error(`장소명을 찾을 수 없습니다: ${placeName}`);
-    error.code = 'NOT_FOUND';
-    throw error;
+    throw new NotExistsError(`장소명을 찾을 수 없습니다: ${placeName}`);
   }
   return {
     lat: places[0].lat,
     lng: places[0].lng,
   };
-};
+};//좌표값 찾는 함수 
 
 export const getWalkingRouteByPlaceNames = async (originName, destinationName, waypointNames = []) => {
+  try{
   console.log('[경로 검색 요청]');
   console.log('originName:', originName);
   console.log('destinationName:', destinationName);
@@ -33,9 +38,7 @@ export const getWalkingRouteByPlaceNames = async (originName, destinationName, w
   console.log('waypoints:', waypoints);
 
   if (origin.lat === destination.lat && origin.lng === destination.lng) {
-    const error = new Error('출발지와 목적지가 동일합니다.');
-    error.code = 'SAME_LOCATION';
-    throw error;
+      throw new InvalidInputError('출발지와 목적지가 동일할 수 없습니다.');
   }
 
   let passList = waypoints
@@ -75,4 +78,11 @@ export const getWalkingRouteByPlaceNames = async (originName, destinationName, w
 
   const nameSummary = [originName, ...waypointNames, destinationName].join(' → ');
   return new RouteResponseDto(response.data, nameSummary);
+
+}catch (error) {
+    if (error.response?.status === 400) {
+      throw new InvalidInputError('Tmap에서 경로를 생성할 수 없습니다. 경유지를 확인해주세요.', error.response?.data);
+    }
+    throw new SampleError('Tmap 요청 중 서버에러가 발생했습니다.', error.message);
+  }
 };
