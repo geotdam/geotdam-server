80bdb61c23007db8c89fefb5e85a12481eabd07b
gimchaewon <gimchaewon417@gmail.com>
2025-05-23 22:04:45 +0900

    🐛 fix: 경로 검색 코드 수정

--- src/dtos/road/road.search.dto.js
+++ src/dtos/road/road.search.dto.js
@@ -1,15 +1,11 @@
 export class RoadSearchDto {
-  constructor({ route_id, name, description, images, reviews, likes }) {
+  constructor({ route_id, name, description, routeImgs, dataValues }) {
     this.routeId = route_id;
     this.name = name;
     this.description = description;
-
-    // 대표 이미지 1개 추출
-    this.thumbnail_url = images?.[0]?.routeImgUrl || null;
-
-    // 리뷰 수, 좋아요 수 계산
-    this.review_count = reviews?.length || 0;
-    this.like_count = likes?.length || 0;
+    this.thumbnail_url = routeImgs?.[0]?.routeImgUrl || null;
+    this.review_count = Number(dataValues?.review_count) || 0;
+    this.like_count = Number(dataValues?.like_count) || 0;
   }
 }
 
