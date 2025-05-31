c66282d88680b02dabf0d66ce3596580f01318c7
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2025-06-01 01:21:29 +0900

    feat: 루트 목록 조회 전체, 본인, 타인 #72

--- src/routes/road/road.routes.js
+++ src/routes/road/road.routes.js
@@ -5,10 +5,28 @@ import { roadSearch } from "../../controllers/road/road.search.controller.js";
 import { createShareLink } from "../../controllers/road/road.share.controller.js";
 import { getRoadDetail } from "../../controllers/road/road.detail.controller.js";
 import * as roadController from "../../controllers/road/road.controller.js";
+import {
+  getMyRoutes,
+  getUserRoutes,
+  getAllRoutes
+} from "../../controllers/road/road.list.controller.js";
+
 const router = express.Router();
 
 // 나만의 루트 생성
 router.post("/", authenticateJWT, roadController.newRoad);
+
+// 루트 검색 페이징
+router.get("/search", authenticateJWT, roadSearch);
+
+// 내가 만든 루트 목록
+router.get("/myroots", authenticateJWT, getMyRoutes);
+
+// 특정 사용자 루트 목록
+router.get("/users/:userId", getUserRoutes);
+
+// 전체 루트 목록 (무한스크롤)
+router.get("/", getAllRoutes);
 
 // 루트 상세 조회
 router.get("/:routeId", authenticateJWT, getRoadDetail);
@@ -21,19 +39,6 @@ router.get("/:routeId/reviews", authenticateJWT, roadController.listReview);
 
 // 루트 리뷰 수정
 router.put("/reviews/:reviewId", authenticateJWT, roadController.updateReview);
-
-/**
- * 잘 못 생각해서.. 필요없는 라우터입니다..~
- *
- * // 루트에 별점 추가하기
- * router.post("/:routeId/rates", authenticateJWT, roadController.newRates);
- *
- * // 루트에 별점 추가했던 거 수정하기
- * router.put("/:routeId/rates", authenticateJWT, roadController.updateRates);
- */
-
-// 루트 검색 페이징
-router.get("/search", authenticateJWT, roadSearch);
 
 //루트 sns 링크 생성
 router.post("/:routeId/share", authenticateJWT, createShareLink);
