8815ddaa03b4524fe9169e69b3967ebc7980c117
chaeyeon089 <chaeyeon0091@naver.com>
2025-06-10 19:13:39 +0900

    Update userImgs.js

--- src/models/database/userImgs.js
+++ src/models/database/userImgs.js
@@ -1,31 +1,27 @@
-//유저 이미지
+//유저 이미지 
 export default (sequelize, DataTypes) => {
-  const UserImgs = sequelize.define(
-    "UserImgs",
-    {
-      userProfileId: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        autoIncrement: true,
-        field: "user_profile_id",
-      },
-      userId: {
-        type: DataTypes.BIGINT,
-        field: "user_id",
-      },
-      imageUrl: {
-        type: DataTypes.STRING,
-        field: "image_url",
-      },
+  const UserImgs = sequelize.define('UserImgs', {
+    userProfileId: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
+      field: 'user_profile_id'
     },
-    {
-      tableName: "userImgs",
-      timestamps: false,
+    userId: {
+      type: DataTypes.BIGINT,
+      field: 'user_id'
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      field: 'image_url'
     }
-  );
+  }, {
+    tableName: 'userImgs',
+    timestamps: false
+  });
 
   UserImgs.associate = (models) => {
-    UserImgs.belongsTo(models.Users, { foreignKey: "userId" });
+  UserImgs.belongsTo(models.Users, { foreignKey: 'userId' });
   };
   return UserImgs;
 };
