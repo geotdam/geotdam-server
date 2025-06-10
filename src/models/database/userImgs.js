3bce9eb722a7cfa2d6b063d66771acd6ccba023b
ian <hope030327@naver.com>
2025-06-10 18:57:31 +0900

    WIP: 장소 북마크 티맵id로 연동

--- src/models/database/userImgs.js
+++ src/models/database/userImgs.js
@@ -1,27 +1,31 @@
-//유저 이미지 
+//유저 이미지
 export default (sequelize, DataTypes) => {
-  const UserImgs = sequelize.define('UserImgs', {
-    userProfileId: {
-      type: DataTypes.BIGINT,
-      primaryKey: true,
-      autoIncrement: true,
-      field: 'user_profile_id'
+  const UserImgs = sequelize.define(
+    "UserImgs",
+    {
+      userProfileId: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        field: "user_profile_id",
+      },
+      userId: {
+        type: DataTypes.BIGINT,
+        field: "user_id",
+      },
+      imageUrl: {
+        type: DataTypes.STRING,
+        field: "image_url",
+      },
     },
-    userId: {
-      type: DataTypes.BIGINT,
-      field: 'user_id'
-    },
-    imageUrl: {
-      type: DataTypes.STRING,
-      field: 'image_url'
+    {
+      tableName: "userImgs",
+      timestamps: false,
     }
-  }, {
-    tableName: 'userimgs',
-    timestamps: false
-  });
+  );
 
   UserImgs.associate = (models) => {
-  UserImgs.belongsTo(models.Users, { foreignKey: 'userId' });
+    UserImgs.belongsTo(models.Users, { foreignKey: "userId" });
   };
   return UserImgs;
 };
