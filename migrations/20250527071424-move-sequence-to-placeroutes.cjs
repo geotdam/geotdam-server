23b4fc2a193b1d47eaccd940501e0df9af4e56c7
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2025-06-10 16:58:46 +0900

    Update 20250527071424-move-sequence-to-placeroutes.cjs

--- migrations/20250527071424-move-sequence-to-placeroutes.cjs
+++ migrations/20250527071424-move-sequence-to-placeroutes.cjs
@@ -2,12 +2,12 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // places 테이블에 sequence 컬럼이 있는지 확인하고 제거
     const placesTable = await queryInterface.describeTable('places');
     if (placesTable.sequence) {
       await queryInterface.removeColumn('places', 'sequence');
     }
-    // 정확한 테이블명 'placeRoutes'로 수정
+
+    // 정확한 테이블명 'placeRoutes'로 변경
     await queryInterface.addColumn('placeRoutes', 'sequence', {
       type: Sequelize.INTEGER,
       allowNull: true,
